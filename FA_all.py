cdab6bb2fc8fc5d4b9f403c08bc88fd96bacabe5
Bohdan Horon <goronbohdan@gmail.com>
2025-05-07 18:24:11 +0300

    fix last word processing

--- FA_all.py
+++ FA_all.py
@@ -262,7 +262,7 @@ def make_markov_chain(data: List, order: int = 1) -> Dict[str, Ngram]:
         for i in range(L):
             item = data[i]
             next_item = data[i + order]
-            
+
             model[item].update([next_item])
             model[item].pos.append(i + order)
             model[item].bool[i] = 1
@@ -273,7 +273,7 @@ def make_markov_chain(data: List, order: int = 1) -> Dict[str, Ngram]:
 
         # З'єднуємо останнє слово з першим та перше з останнім
         model[data[L]].update([data[0]])
-        if data[L] not in model[data[L]].pos:
+        if data[L] not in model:
             model[data[L]].pos.append(L + order)
             model[data[L]].bool = np.zeros(L, dtype=np.uint8)
             model[data[L]].bool[L-1] = 1
